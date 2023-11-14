ba4599bf268044982ddbabd83a02293510e40bd1
Yackie Burch <jaclynnburch@gmail.com>
2023-11-13 16:38:33 -0800

    added search history function

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function handleSearch(event) {
   
     fetchWeatherData(searchTerm);
   }
+  function updateSearchHistory(searchTerm) {
+    /// Get the search history container
+    const updateSearchHistoryContainer = document.getElemeentByID("searchHistory");
+
+    // Create a new div element for the recent search
+    const searchItem = document.createElement("div");
+    searchItem.className = "search-item bg-gradient mt-3 fw-bold text-center py-2 rounded";
+    searchItem.textContent = searchTerm;
+
+    // Inset the new div at the beginning of the search history container
+    searchHisrtoryContainer.insertBefore(
+        searchItem,
+        SearchHistoryContainer.firstChild
+    );
+  }
+
+  async function fetchWeatherData(cityName) {
+    const apiKey = "6b113faae5c375b923daf36b5859653e"
+  }
